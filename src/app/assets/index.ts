1315adb90a8790417b85f043206b9f9fac0e8bea
Andrewcephas <ngumbaucephas2@gmail.com>
2026-04-16 20:57:14 +0300

    working mvp

---
+++ src/app/assets/index.ts
@@ -0,0 +1,28 @@
+const images = {
+  heroImages: {
+    obinnaProfile: '/src/assets/images/obinna-profile.jpg',
+    ethicEnt: '/src/assets/images/ethic-ent.jpg',
+    demWaFb: '/src/assets/images/dem-wa-fb.jpg',
+    willyPaul: '/src/assets/images/Willy-Paul-msafi.jpg'
+  },
+  aboutImages: {
+    studioBts: '/src/assets/images/studio-bts-scaled.jpg',
+    founder: '/src/assets/images/obinna-profile.jpg',
+    contentCreation: '/src/assets/images/content-creation.jpg'
+  },
+  newsImages: {
+    FridaLofty: '/src/assets/images/IMG_3810-scaled.jpg',
+    Vurugu: '/src/assets/images/Vurugu-Design.jpg-1.jpeg',
+    HochMuhoro: '/src/assets/images/IMG_4305-scaled.jpg'
+  },
+  galleryImages: {},
+  videoThumbnails: {
+    latest: '/src/assets/images/studio-bts-scaled.jpg',
+    interview: '/src/assets/images/IMG_9197-scaled.jpg',
+    podcast: '/src/assets/images/obtv-bts-1.jpg',
+    comedy: '/src/assets/images/IMG_4305-scaled.jpg',
+    performance: '/src/assets/images/Willy-Paul-msafi.jpg',
+    studio: '/src/assets/images/studio-bts-scaled.jpg'
+  }
+};
+export default images;
